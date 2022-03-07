0a047a93cb32ee3f128e8f990889a7efd843a3e9
Milinkov-N <milinkov.nik@gmail.com>
2022-03-07 16:04:48 +0400

    added related products to product pages

--- pages/products/[product_id].jsx
+++ pages/products/[product_id].jsx
@@ -6,6 +6,7 @@ import { getSingleProduct, recursiveCatalog } from '../../src/utils'
 import useProduct from '../../src/hooks/useProduct'
 
 import styles from '../../styles/productpage.module.css'
+import RelatedProducts from '../../src/components/RelatedProducts'
 
 export default function ProductPage({ product }) {
   const {
@@ -17,37 +18,39 @@ export default function ProductPage({ product }) {
 
   return (
     <Layout>
-      <Container className={ styles.grid }>
-        <div className={ styles.productImage }>
-          <img src={ product?.images?.edges[0]?.node?.transformedSrc } alt="product image" />
-        </div>
-        <div className={ styles.info }>
-          <h2 className={ `${styles.title} heading-2` }>
-            <span>{ product?.title }</span>
-            { setTags(product.tags).map( item => {
-                return item
-              })
-            }
-          </h2>
-          <h3 className={ styles.price }>{ `${ Math.floor(product.priceRange.minVariantPrice.amount) } RUB` }</h3>
-          <div className={ styles.description } dangerouslySetInnerHTML={{ __html: product.descriptionHtml }} />
-          <div className={ styles.action }>
-            <QuantitySelector
-              quantity={ quantity }
-              setQuantity={ setQuantity }
-            />
-            <Button
-              variant='primary'
-              size='medium'
-              label='В корзину'
-              onClick={ () => {
-                handleAddToCart(product.id, quantity)
-                setQuantity(1)
-              }}
-            />
+      <Container>
+        <div className={ styles.grid }>
+          <div className={ styles.productImage }>
+            <img src={ product?.images?.edges[0]?.node?.transformedSrc } alt="product image" />
+          </div>
+          <div className={ styles.info }>
+            <h2 className={ `${styles.title} heading-2` }>
+              <span>{ product?.title }</span>
+              { setTags(product.tags).map( item => {
+                  return item
+                })
+              }
+            </h2>
+            <h3 className={ styles.price }>{ `${ Math.floor(product.priceRange.minVariantPrice.amount) } RUB` }</h3>
+            <div className={ styles.description } dangerouslySetInnerHTML={{ __html: product.descriptionHtml }} />
+            <div className={ styles.action }>
+              <QuantitySelector
+                quantity={ quantity }
+                setQuantity={ setQuantity }
+              />
+              <Button
+                variant='primary'
+                size='medium'
+                label='В корзину'
+                onClick={ () => {
+                  handleAddToCart(product.id, quantity)
+                  setQuantity(1)
+                }}
+              />
+            </div>
           </div>
         </div>
-        <div className="related-products"></div>
+        <RelatedProducts />
       </Container>
     </Layout>
   )
---
+++ src/components/RelatedProducts.jsx
@@ -0,0 +1,107 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+// import required modules
+import { Pagination } from 'swiper'
+
+import styles from './style_modules/RelatedProducts.module.css'
+import Product from './Product'
+import { storefront } from '../../src/utils'
+import { useEffect, useState } from 'react'
+
+export default function RelatedProducts() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() =>{
+    async function getProducts() {
+      const { data } = await storefront(query)
+
+      setProducts(data.collection.products.edges)
+    }
+
+    getProducts()
+  },[])
+
+  return (
+    <div className={ styles.wrapper }>
+      <h2 className='heading-2'>Рекомендуем</h2>
+      <Swiper
+        className={ styles.swiper }
+        slidesPerView={ 1 }
+        spaceBetween={ 10 }
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          480: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          620: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          1200: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination]}
+      >
+        {
+          products.map(product => {
+            return (
+              <SwiperSlide className={ styles.swiperSlide }>
+                <Product
+                  key={ product.node.id }
+                  data={ product }
+                />
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
+    </div>
+    
+  )
+}
+
+const query = `
+  query relatedProducts {
+    collection(handle: "related-products") {
+      handle
+      id
+      descriptionHtml
+      products(first: 250) {
+        edges {
+          node {
+            id
+            handle
+            title
+            description
+            images(first: 1) {
+              edges {
+                node {
+                  transformedSrc
+                  altText
+                }
+              }
+            }
+            tags
+            priceRange {
+              minVariantPrice {
+                amount
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
