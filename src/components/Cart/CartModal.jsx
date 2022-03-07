7e092007f8c1ae26c61f4f62bd77328edc8c0d42
Milinkov-N <milinkov.nik@gmail.com>
2022-03-07 20:11:34 +0400

    added related products to cart

--- src/components/Cart/CartModal.jsx
+++ src/components/Cart/CartModal.jsx
@@ -1,8 +1,14 @@
+import Image from 'next/image'
+
 import OrderSummary from './OrderSummary'
 import CartList from './CartList'
 import Button from '../ui/Button'
 import { useCart } from '../../contexts/CartContext'
+import LogoImg from '../../../public/pasta.jpg'
 
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { FreeMode, } from 'swiper'
+import 'swiper/css'
 import styles from '../style_modules/cartmodal.module.css'
 
 export default function CartModal() {
@@ -19,7 +25,88 @@ export default function CartModal() {
     <div className={ `${ styles.cartModal } ${ openedClass }` }>
       <div className={ styles.grid }>
         <CartModalHeader handler={ handler } />
-        <CartList />
+        <div className={ styles.scroll }>
+          <CartList />
+          <div className="add-to-order">
+            <h3 className='heading-4' style={{ marginBottom: '.75em' }}>Добавить к заказу?</h3>
+            <Swiper
+              className={ styles.swiper }
+              slidesPerView={ 'auto' }
+              spaceBetween={ 20 }
+              freeMode={ true }
+              modules={[FreeMode]}
+            >
+              <SwiperSlide className={ styles.swiperSlide }>
+                <div className={ styles.addToOrderItem }>
+                  <div className={ styles.imgWrapper }>
+                    <Image
+                      priority
+                      src={ LogoImg.src }
+                      alt={ 'img' }
+                      layout='fill'
+                      objectFit='contain' 
+                    />
+                  </div>
+                  <div>
+                    <h4>Кока-кола</h4>
+                    <span>200 RUB</span>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide className={ styles.swiperSlide }>
+                <div className={ styles.addToOrderItem }>
+                  <div className={ styles.imgWrapper }>
+                    <Image
+                      priority
+                      src={ LogoImg.src }
+                      alt={ 'img' }
+                      layout='fill'
+                      objectFit='contain' 
+                    />
+                  </div>
+                  <div>
+                    <h4>Фанта</h4>
+                    <span>200 RUB</span>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide className={ styles.swiperSlide }>
+                <div className={ styles.addToOrderItem }>
+                  <div className={ styles.imgWrapper }>
+                    <Image
+                      priority
+                      src={ LogoImg.src }
+                      alt={ 'img' }
+                      layout='fill'
+                      objectFit='contain' 
+                    />
+                  </div>
+                  <div>
+                    <h4 className='text'>Картошка фри</h4>
+                    <span>200 RUB</span>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide className={ styles.swiperSlide }>
+                <div className={ styles.addToOrderItem }>
+                  <div className={ styles.imgWrapper }>
+                    <Image
+                      priority
+                      src={ LogoImg.src }
+                      alt={ 'img' }
+                      layout='fill'
+                      objectFit='contain' 
+                    />
+                  </div>
+                  <div>
+                    <h4>Соус</h4>
+                    <span>200 RUB</span>
+                  </div>
+                </div>
+              </SwiperSlide>
+            </Swiper>
+          </div>
+        </div>
         <OrderSummary />
       </div>
     </div>
